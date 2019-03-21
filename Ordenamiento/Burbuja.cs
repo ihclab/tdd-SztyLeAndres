ed8d8a5530283dea54ab3a6d9166bd0939628231
Andres Martinez <amartinez31@ucol.mx>
2019-03-21 09:54:06 -0600

    Estructura de Ordenamiento de Burbuja

---
+++ Ordenamiento/Burbuja.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ordenamiento
+{
+    public class Class1
+    {
+        public void Ordenar(int[] arr)
+        {
+            throw new NotImplementedException("Crear los casos de prueba");
+        }
+    }
+}
